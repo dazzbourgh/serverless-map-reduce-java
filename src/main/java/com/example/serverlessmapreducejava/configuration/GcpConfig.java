881980f69466f5d4b0fcfff6c4164b2d5a70753e
lborisevich-clgx <lborisevich@corelogic.com>
2020-06-16 18:07:55 -0700

    added storage implementation

--- src/main/java/com/example/serverlessmapreducejava/configuration/GcpConfig.java
+++ src/main/java/com/example/serverlessmapreducejava/configuration/GcpConfig.java
@@ -3,6 +3,8 @@ package com.example.serverlessmapreducejava.configuration;
 import com.google.cloud.bigquery.BigQuery;
 import com.google.cloud.bigquery.BigQueryFactory;
 import com.google.cloud.bigquery.BigQueryOptions;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +14,10 @@ public class GcpConfig {
     public BigQuery bigQuery() {
         BigQueryFactory bigQueryFactory = new BigQueryOptions.DefaultBigQueryFactory();
         return bigQueryFactory.create(BigQueryOptions.newBuilder().build());
+    }
+
+    @Bean
+    public Storage storage() {
+        return StorageOptions.newBuilder().build().getService();
     }
 }
--- src/main/java/com/example/serverlessmapreducejava/functions/business/BusinessLogic.java
+++ src/main/java/com/example/serverlessmapreducejava/functions/business/BusinessLogic.java
@@ -3,16 +3,11 @@ package com.example.serverlessmapreducejava.functions.business;
 import com.example.serverlessmapreducejava.domain.Animal;
 import com.example.serverlessmapreducejava.domain.Classification;
 import com.example.serverlessmapreducejava.utils.queue.QueueSender;
-import lombok.SneakyThrows;
+import com.example.serverlessmapreducejava.utils.storage.StorageService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Stream;
@@ -20,24 +15,22 @@ import java.util.stream.Stream;
 @Component
 public class BusinessLogic {
     private final Consumer<Animal> consume;
+    private final StorageService storageService;
 
-    public BusinessLogic(QueueSender queueSender, @Value("${topic}") String topic) {
+    public BusinessLogic(QueueSender queueSender,
+                         @Value("${topic}") String topic,
+                         StorageService storageService) {
+        this.storageService = storageService;
         this.consume = animal -> queueSender.send(animal, topic);
     }
 
     @Bean
     public Consumer<String> read() {
         return pathString -> {
-            Path path = Paths.get(URI.create(pathString));
-            try (Stream<String> lines = getLines(path)) {
+            try (Stream<String> lines = storageService.get(pathString)) {
                 lines.map(toAnimal()).forEach(consume);
             }
         };
-    }
-
-    @SneakyThrows
-    private Stream<String> getLines(Path path) {
-        return Files.lines(path);
     }
 
     @Bean
